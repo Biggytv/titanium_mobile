ea0ae1eb76f3aeecfa5d6bc8750edb9612196e2e
root <root@passkey.(none)>
2010-03-31 09:29:35 +0800

    Refactor Spinner

--- android/modules/ui/src/ti/modules/titanium/ui/widget/TiUISpinner.java
+++ android/modules/ui/src/ti/modules/titanium/ui/widget/TiUISpinner.java
@@ -34,7 +34,7 @@ public class TiUISpinner extends TiUIView implements OnItemSelectedListener
 	public TiUISpinner(TiViewProxy proxy) {
 		super(proxy);
 		if (DBG) {
-			Log.d(LCAT, "Creating a text text");
+			Log.d(LCAT, "Creating a spinner");
 		}
 		//TextView tv = new TextView(getProxy().getContext());
 		s = new Spinner(getProxy().getContext());
@@ -47,74 +47,31 @@ public class TiUISpinner extends TiUIView implements OnItemSelectedListener
 	{
 		super.processProperties(d);
 
-		Spinner tv = (Spinner) getNativeView();
 		if (d.containsKey("options")) {
 			String[] optionText = d.getStringArray("options");
 			Log.d(LCAT,"Processing options");
-			//processOptions(optionText);
 			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
 		            getProxy().getContext(), android.R.layout.simple_spinner_item, optionText);
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		    
-			tv.setAdapter(adapter);
+			s.setAdapter(adapter);
 		    
 		}
 		if (d.containsKey("prompt")) {
 			String prompt = d.getString("prompt");
-			tv.setPrompt(prompt);
+			s.setPrompt(prompt);
 		}
-		tv.invalidate();
-	}
-
-	private void processOptions(String[] optionText) {
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-	            getProxy().getContext(), android.R.layout.simple_spinner_dropdown_item,
-	            new String[] { "Apple", "Peach", "Banana" });
-		
-	    Spinner tv = (Spinner) getNativeView();
-	    tv.setAdapter(adapter);
-	}
-	
-	private void setAlignment(TextView tv, String textAlign) {
-		if ("left".equals(textAlign)) {
-			tv.setGravity(Gravity.TOP | Gravity.LEFT);
-		} else if ("center".equals(textAlign)) {
-			tv.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
-		} else if ("right".equals(textAlign)) {
-			tv.setGravity(Gravity.TOP | Gravity.RIGHT);
-		} else {
-			Log.w(LCAT, "Unsupported alignment: " + textAlign);
-		}
+		s.invalidate();
 	}
 
 	@Override
 	public void propertyChanged(String key, Object oldValue, Object newValue, TiProxy proxy)
 	{
-		if (DBG) {
-			Log.d(LCAT, "Property: " + key + " old: " + oldValue + " new: " + newValue);
-		}
-		TextView tv = (TextView) getNativeView();
-		if (key.equals("text")) {
-			tv.setText(TiConvert.toString(newValue));
-			tv.requestLayout();
-		} else if (key.equals("color")) {
-			tv.setTextColor(TiConvert.toColor((String) newValue));
-		} else if (key.equals("highlightedColor")) {
-			tv.setHighlightColor(TiConvert.toColor((String) newValue));
-		} else if (key.equals("textAlign")) {
-			setAlignment(tv, (String) newValue);
-			tv.requestLayout();
-		} else if (key.equals("font")) {
-			TiUIHelper.styleText(tv, (TiDict) newValue);
-			tv.requestLayout();
-		} else {
-			super.propertyChanged(key, oldValue, newValue, proxy);
-		}
+		//TODO: implement property change handler
 	}
 
 	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-			long arg3) {
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		String value = s.getSelectedItem().toString();
 		TiDict data = new TiDict();
 		data.put("value", value);
@@ -125,7 +82,6 @@ public class TiUISpinner extends TiUIView implements OnItemSelectedListener
 
 	@Override
 	public void onNothingSelected(AdapterView<?> arg0) {
-		
-		//proxy.internalSetDynamicValue("value", value, fireChange)
+		//Do nothing
 	}
 }
