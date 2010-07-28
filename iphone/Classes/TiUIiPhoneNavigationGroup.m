efefddcbf8b92a47977d57ecd3e36970d1cd4b18
Stephen Tramer <sptramer@gmail.com>
2010-07-29 00:14:13 +0800

    Fix for navigation group redraw issue when reopening.

--- iphone/Classes/TiUIiPhoneNavigationGroup.m
+++ iphone/Classes/TiUIiPhoneNavigationGroup.m
@@ -113,10 +113,6 @@
 	TiWindowViewController *wincontroller = (TiWindowViewController*)viewController;
 	TiWindowProxy *newWindow = [wincontroller proxy];
 	[self setVisibleProxy:newWindow];
-	if (newWindow==current || newWindow==root)
-	{
-		return;
-	}
 	
 	if (newWindow!=current)
 	{
