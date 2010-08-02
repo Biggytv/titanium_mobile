6f7a1a2d65b54e03f01254aa4f339a90b1d5fb5e
donthorp <don@donthorp.net>
2010-08-03 06:21:42 +0800

    [#1446 state:fixed-in-qa] background widget was not properly invalidating itself when pressed. Still have no idea why all of the states didn't fail

--- android/titanium/src/org/appcelerator/titanium/view/TiBackgroundDrawable.java
+++ android/titanium/src/org/appcelerator/titanium/view/TiBackgroundDrawable.java
@@ -101,6 +101,7 @@ public class TiBackgroundDrawable extends StateListDrawable {
 	@Override
 	protected boolean onStateChange(int[] stateSet) {
 		boolean changed = super.onStateChange(stateSet);
+		changed = setState(stateSet);
 		boolean drawableChanged = false;
 		if (background != null) {
 //			Log.e("TiBackground", "background="+background.getClass().getSimpleName()+",state.len="+stateSet.length);
@@ -108,9 +109,9 @@ public class TiBackgroundDrawable extends StateListDrawable {
 //				Log.e("TiBackground", "    state[" + i + "]=" + stateSet[i]);
 //			}
 			drawableChanged = background.setState(stateSet);
-//			if (drawableChanged) {
-//				background.invalidateSelf();
-//			}
+			if (drawableChanged) {
+				invalidateSelf();
+			}
 		}
 
 		return changed || drawableChanged;
