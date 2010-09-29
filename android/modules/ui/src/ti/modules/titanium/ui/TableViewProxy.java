dc53c1564be909634de5e627aa781b16c470731d
billdawson <bill@billdawson.com>
2010-09-30 00:01:03 +0800

    [#1945 state:fixed-in-qa] Synchronize methods that both insert/delete rows (or replace the row data completely) and call updateView, so that the data cannot be again changed while updateView is still running.

--- android/modules/ui/src/ti/modules/titanium/ui/TableViewProxy.java
+++ android/modules/ui/src/ti/modules/titanium/ui/TableViewProxy.java
@@ -104,7 +104,7 @@ public class TableViewProxy extends TiViewProxy
 		}
 	}
 
-	public void appendRow(Object row, TiDict options)
+	public synchronized void appendRow(Object row, TiDict options)
 	{
 		TableViewRowProxy rowProxy = rowProxyFor(row);
 		
@@ -124,7 +124,7 @@ public class TableViewProxy extends TiViewProxy
 		updateView();
 	}
 
-	public void deleteRow(int index, TiDict options)
+	public synchronized void deleteRow(int index, TiDict options)
 	{
 		RowResult rr = new RowResult();
 		if (locateIndex(index, rr)) {
@@ -159,7 +159,7 @@ public class TableViewProxy extends TiViewProxy
 		return index;
 	}
 
-	public void insertRowBefore(int index, Object data, TiDict options) {
+	public synchronized void insertRowBefore(int index, Object data, TiDict options) {
 		if (getSections().size() > 0) {
 			if (index < 0) {
 				index = 0;
@@ -182,7 +182,7 @@ public class TableViewProxy extends TiViewProxy
 		updateView();
 	}
 
-	public void insertRowAfter(int index, Object data, TiDict options) {
+	public synchronized void insertRowAfter(int index, Object data, TiDict options) {
 		RowResult rr = new RowResult();
 		if (locateIndex(index, rr)) {
 			// TODO check for section
@@ -264,7 +264,7 @@ public class TableViewProxy extends TiViewProxy
 		}
 	}
 
-	public void setData(Object[] data, TiDict options) {
+	public synchronized void setData(Object[] data, TiDict options) {
 		if (data != null) {
 			processData(data);
 			getTableView().setModelDirty();
